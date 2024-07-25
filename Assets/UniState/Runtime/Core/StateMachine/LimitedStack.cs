1aded80e970219f8e3665382e41e342ac0af01bb
Artsiomi Silivonchyk <mr.hant@gmail.com>
2024-07-25 13:36:53 +0400

    Change LimitedStack to use sliding array implementation

--- Assets/UniState/Runtime/Core/StateMachine/LimitedStack.cs
+++ Assets/UniState/Runtime/Core/StateMachine/LimitedStack.cs
@@ -1,37 +1,46 @@
-using System.Collections.Generic;
-
 namespace UniState
 {
     public class LimitedStack<T>
     {
-        private readonly List<T> _list;
+        private readonly  T[] _list;
+        private int _top = 0;
+        private int _bot = 0;
+        private int _maxSize;
+        // count: (_top - _bot) % _maxSize
+        // top index: (_top - 1) % _maxSize
+        // isNotEmpty: _top != _bot
 
         public LimitedStack(int maxSize)
         {
-            _list = new List<T>(maxSize);
+            _maxSize = maxSize;
+            _list = new T[_maxSize];
         }
 
         public T Push(T element)
         {
-            if (_list.Capacity == _list.Count)
+            // If max capacity reached
+            // remove one from bottom
+            // no need to clear as it will be replaced right away
+            if (_top != _bot && (_top - _bot) % _maxSize == 0)
             {
-                _list.RemoveAt(0);
+                _bot++;
             }
 
-            _list.Add(element);
-
+            _top++;
+            _list[(_top-1)%_maxSize] = element;
             return element;
         }
 
-        public T Peek() => _list.Count > 0 ? _list[^1] : default;
+        public T Peek() => _top != _bot ? _list[(_top-1)%_maxSize] : default(T);
 
         public T Pop()
         {
             var result = Peek();
 
-            if (_list.Count > 0)
+            if (_top != _bot)
             {
-                _list.RemoveAt(_list.Count - 1);
+                _list[(_top-1)%_maxSize] = default(T);
+                _top--;
             }
 
             return result;
