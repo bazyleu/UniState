86d934523464e9599ba23c1d9d95e41e2201b2d6
Dzmitry Bazyleu <d.bazylev@gmail.com>
2025-02-07 18:20:29 +0100

    Add VContainer and Zenject extensions for singletons

--- Assets/UniState/Runtime/Integrations/VContainer/VContainerBuilderExtensions.cs
+++ Assets/UniState/Runtime/Integrations/VContainer/VContainerBuilderExtensions.cs
@@ -8,19 +8,39 @@ namespace UniState
     {
         public static RegistrationBuilder RegisterStateMachine<TStateMachine>(this IContainerBuilder builder)
             where TStateMachine : IStateMachine =>
-            builder.Register<TStateMachine>(Lifetime.Transient);
+            builder.RegisterStateMachine<TStateMachine>(Lifetime.Transient);
 
-        public static RegistrationBuilder RegisterAbstractStateMachine<TStateMachineBase, TStateMachine>(this IContainerBuilder builder)
+        public static RegistrationBuilder RegisterStateMachine<TStateMachine>(this IContainerBuilder builder,
+            Lifetime lifetime)
+            where TStateMachine : IStateMachine =>
+            builder.Register<TStateMachine>(lifetime);
+
+        public static RegistrationBuilder RegisterAbstractStateMachine<TStateMachineBase, TStateMachine>(
+            this IContainerBuilder builder)
             where TStateMachine : TStateMachineBase
             where TStateMachineBase : IStateMachine =>
-            builder.Register<TStateMachineBase, TStateMachine>(Lifetime.Transient);
+            builder.RegisterAbstractStateMachine<TStateMachineBase, TStateMachine>(Lifetime.Transient);
+
+        public static RegistrationBuilder RegisterAbstractStateMachine<TStateMachineBase, TStateMachine>(
+            this IContainerBuilder builder, Lifetime lifetime)
+            where TStateMachine : TStateMachineBase
+            where TStateMachineBase : IStateMachine =>
+            builder.Register<TStateMachineBase, TStateMachine>(lifetime);
 
         public static RegistrationBuilder RegisterState<TState>(this IContainerBuilder builder) =>
-            builder.Register<TState>(Lifetime.Transient).AsSelf().AsImplementedInterfaces();
+            builder.RegisterState<TState>(Lifetime.Transient);
+
+        public static RegistrationBuilder RegisterState<TState>(this IContainerBuilder builder, Lifetime lifetime) =>
+            builder.Register<TState>(lifetime).AsSelf().AsImplementedInterfaces();
 
         public static RegistrationBuilder RegisterAbstractState<TStateBase, TState>(this IContainerBuilder builder)
             where TState : TStateBase =>
-            builder.Register<TStateBase, TState>(Lifetime.Transient);
+            builder.RegisterAbstractState<TStateBase, TState>(Lifetime.Transient);
+
+        public static RegistrationBuilder RegisterAbstractState<TStateBase, TState>(this IContainerBuilder builder,
+            Lifetime lifetime)
+            where TState : TStateBase =>
+            builder.Register<TStateBase, TState>(lifetime);
     }
 }
 
