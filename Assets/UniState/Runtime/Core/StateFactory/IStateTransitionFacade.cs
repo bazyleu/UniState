df8f8165f388f734c33eed099ff9e247c8331e52
JakBlack <blackjak197@gmail.com>
2025-04-13 22:01:48 +0200

    Pull up GoBackTo method to IStateTransitionFacade

--- Assets/UniState/Runtime/Core/StateFactory/IStateTransitionFacade.cs
+++ Assets/UniState/Runtime/Core/StateFactory/IStateTransitionFacade.cs
@@ -9,6 +9,8 @@ namespace UniState
             where TState : class, IState<EmptyPayload>;
 
         StateTransitionInfo GoBack();
+        StateTransitionInfo GoBackTo<TState>()
+            where TState : class, IExecutableState;
         StateTransitionInfo GoToExit();
     }
 }
