923bd4e159c93a758da42d37cb13d6d5d6380112
Dzmitry Bazyleu <d.bazylev@gmail.com>
2025-04-18 20:57:11 +0200

    Update tutorial section

--- Assets/Examples/States/LostState.cs
+++ Assets/Examples/States/LostState.cs
@@ -11,10 +11,13 @@ namespace Examples.States
         public override async UniTask<StateTransitionInfo> Execute(CancellationToken token)
         {
             Debug.Log("You lost. You will have a another chance in...");
+
             Debug.Log("3 seconds");
             await UniTask.Delay(TimeSpan.FromSeconds(1), cancellationToken: token);
+
             Debug.Log("2 seconds");
             await UniTask.Delay(TimeSpan.FromSeconds(1), cancellationToken: token);
+
             Debug.Log("1 second");
             await UniTask.Delay(TimeSpan.FromSeconds(1), cancellationToken: token);
 
